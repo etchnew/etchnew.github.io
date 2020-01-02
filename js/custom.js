3386abab8d83aafbee39c608f7cf6402b18474bd
Frost <frostfury7@gmail.com>
2020-01-02 14:58:55 +0530

    Js update Custom removed elements from index-fixed mobile veiw menu

--- js/custom.js
+++ js/custom.js
@@ -26,6 +26,8 @@ $(document).ready(function()
 	var header = $('.header');
 	var hamburgerBar = $('.hamburger_bar');
 	var hamburger = $('.hamburger');
+	var ctrl = new ScrollMagic.Controller();
+
 
 	setHeader();
 
@@ -47,6 +49,7 @@ $(document).ready(function()
 	initVideo();
 	initGallery();
 	initMenu();
+	initMilestones();
 
 	/* 
 
@@ -135,6 +138,61 @@ $(document).ready(function()
 					}
 				}
 			});
+		}
+	}
+	/* 
+
+	6. Init Milestones
+
+	*/
+	function initMilestones()
+	{
+		if($('.milestone_counter').length)
+		{
+			var milestoneItems = $('.milestone_counter');
+
+	    	milestoneItems.each(function(i)
+	    	{
+	    		var ele = $(this);
+	    		var endValue = ele.data('end-value');
+	    		var eleValue = ele.text();
+
+	    		/* Use data-sign-before and data-sign-after to add signs
+	    		infront or behind the counter number */
+	    		var signBefore = "";
+	    		var signAfter = "";
+
+	    		if(ele.attr('data-sign-before'))
+	    		{
+	    			signBefore = ele.attr('data-sign-before');
+	    		}
+
+	    		if(ele.attr('data-sign-after'))
+	    		{
+	    			signAfter = ele.attr('data-sign-after');
+	    		}
+
+	    		var milestoneScene = new ScrollMagic.Scene({
+		    		triggerElement: this,
+		    		triggerHook: 'onEnter',
+		    		reverse:false
+		    	})
+		    	.on('start', function()
+		    	{
+		    		var counter = {value:eleValue};
+		    		var counterTween = TweenMax.to(counter, 4,
+		    		{
+		    			value: endValue,
+		    			roundProps:"value", 
+						ease: Circ.easeOut, 
+						onUpdate:function()
+						{
+							document.getElementsByClassName('milestone_counter')[i].innerHTML = signBefore + counter.value + signAfter;
+						}
+		    		});
+		    	})
+			    .addTo(ctrl);
+	    	});
 		}
 	}
 
